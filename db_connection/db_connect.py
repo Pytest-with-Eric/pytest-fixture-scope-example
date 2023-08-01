49cfd06c0f213ba359d6750bca277cea6a6b6fa6
Ahmed Waheed <mrcreamio@outlook.com>
2023-08-01 09:02:43 +0500

    added a db connection id

--- db_connection/db_connect.py
+++ db_connection/db_connect.py
@@ -1,5 +1,8 @@
+import random
+
 class MockDB:
     def __init__(self):
+        self.id = random.randint(1, 1000)
         self.connection = "Connected"
 
     def disconnect(self):
--- tests/unit/test_db.py
+++ tests/unit/test_db.py
@@ -1,12 +1,32 @@
-def test_db_function(db_function):
+def test_db_function_1(db_function):
+    print(f"Test with function-scope fixture, id: {db_function.id}")
     assert db_function.connection == "Connected"
 
-def test_db_module(db_module):
+def test_db_function_2(db_function):
+    print(f"Test with function-scope fixture, id: {db_function.id}")
+    assert db_function.connection == "Connected"
+
+def test_db_module_1(db_module):
+    print(f"Test with module-scope fixture, id: {db_module.id}")
+    assert db_module.connection == "Connected"
+
+def test_db_module_2(db_module):
+    print(f"Test with module-scope fixture, id: {db_module.id}")
     assert db_module.connection == "Connected"
 
 class TestDBClass:
-    def test_db_class(self, db_class):
+    def test_db_class_1(self, db_class):
+        print(f"Test with class-scope fixture, id: {db_class.id}")
         assert db_class.connection == "Connected"
 
-def test_db_session(db_session):
+    def test_db_class_2(self, db_class):
+        print(f"Test with class-scope fixture, id: {db_class.id}")
+        assert db_class.connection == "Connected"
+
+def test_db_session_1(db_session):
+    print(f"Test with session-scope fixture, id: {db_session.id}")
+    assert db_session.connection == "Connected"
+
+def test_db_session_2(db_session):
+    print(f"Test with session-scope fixture, id: {db_session.id}")
     assert db_session.connection == "Connected"
