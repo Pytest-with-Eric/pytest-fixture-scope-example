7479e0d172610e2ea27c715ce9a0baf8823f1d72
Eric Sales De Andrade <eric.salesdeandrade@joinstudyhall.com>
2023-08-07 22:16:04 +0530

    Working fixture scope

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,16 +8,3 @@ def smtp_connection():
     print("SMTP Connection fixture start")
     yield smtplib.SMTP("smtp.gmail.com", 587, timeout=60)
     print("SMTP Connection Tear Down")
-
-
-# @pytest.fixture(scope='function', autouse=True)
-# def smtp_connection_function():
-#     print("Function fixture start")
-#     yield smtplib.SMTP("smtp.gmail.com", 587, timeout=60)
-#     print("Function fixture end")
-
-# @pytest.fixture(scope='class', autouse=True)
-# def smtp_connection_class():
-#     print("Class fixture start")
-#     yield smtplib.SMTP("smtp.gmail.com", 587, timeout=60)
-#     print("Class fixture end")
--- tests/test_smtp2.py
+++ tests/test_smtp2.py
@@ -1,25 +1,25 @@
 def test_ehlo(smtp_connection):
     response, msg = smtp_connection.ehlo()
-    print("Test EHLO 1 ")
+    print("Test EHLO 2 ")
     assert response == 250
     assert b"smtp.gmail.com" in msg
 
 
 def test_noop(smtp_connection):
     response, msg = smtp_connection.noop()
-    print("Test NOOP 1 ")
+    print("Test NOOP 2 ")
     assert response == 250
     assert b"OK" in msg
 
-class TestSMTP1:
+class TestSMTP2:
     def test_ehlo(self, smtp_connection):
         response, msg = smtp_connection.ehlo()
-        print("Test EHLO 1 in Class...")
+        print("Test EHLO 2 in Class...")
         assert response == 250
         assert b"smtp.gmail.com" in msg
 
     def test_noop(self, smtp_connection):
         response, msg = smtp_connection.noop()
-        print("Test NOOP 1 in Class...")
+        print("Test NOOP 2 in Class...")
         assert response == 250
         assert b"OK" in msg
