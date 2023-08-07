81c90b6d6d6a758eb868b43b2bbab57196a82bdc
Eric Sales De Andrade <eric.salesdeandrade@joinstudyhall.com>
2023-08-07 22:19:17 +0530

    Code docs

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,13 @@ import smtplib
 
 
 @pytest.fixture(scope='function', autouse=True)
-def smtp_connection():
+def smtp_connection() -> smtplib.SMTP:
+    """
+    A fixture to create an SMTP connection.
+    
+    Returns:
+        An SMTP connection
+    """
     print("SMTP Connection fixture start")
     yield smtplib.SMTP("smtp.gmail.com", 587, timeout=60)
     print("SMTP Connection Tear Down")
--- tests/test_smtp.py
+++ tests/test_smtp.py
@@ -1,24 +1,60 @@
-def test_ehlo(smtp_connection):
+def test_ehlo(smtp_connection) -> None:
+    """
+    Test the SMTP EHLO command
+    
+    Args:
+        smtp_connection: A fixture to create an SMTP connection
+        
+    Returns:
+        None
+    """
     response, msg = smtp_connection.ehlo()
     print("Test EHLO 1 ")
     assert response == 250
     assert b"smtp.gmail.com" in msg
 
 
-def test_noop(smtp_connection):
+def test_noop(smtp_connection) -> None:
+    """
+    Test the SMTP NOOP command
+    
+    Args:
+        smtp_connection: A fixture to create an SMTP connection
+        
+    Returns:
+        None
+    """
     response, msg = smtp_connection.noop()
     print("Test NOOP 1 ")
     assert response == 250
     assert b"OK" in msg
 
 class TestSMTP1:
-    def test_ehlo(self, smtp_connection):
+    def test_ehlo(self, smtp_connection) -> None:
+        """
+        Test the SMTP EHLO command in a class
+        
+        Args:
+            smtp_connection: A fixture to create an SMTP connection
+            
+        Returns:
+            None
+        """
         response, msg = smtp_connection.ehlo()
         print("Test EHLO 1 in Class...")
         assert response == 250
         assert b"smtp.gmail.com" in msg
 
-    def test_noop(self, smtp_connection):
+    def test_noop(self, smtp_connection) -> None:
+        """
+        Test the SMTP NOOP command in a class
+        
+        Args:
+            smtp_connection: A fixture to create an SMTP connection
+            
+        Returns:
+            None
+        """
         response, msg = smtp_connection.noop()
         print("Test NOOP 1 in Class...")
         assert response == 250
