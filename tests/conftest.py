a271791b6ba509ae1b3b79fff9b6fc468b3d4955
Eric Sales De Andrade <eric.salesdeandrade@joinstudyhall.com>
2023-08-11 12:30:59 +0530

    Shared fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,25 @@
 """Conftest file to define fixtures at different scopes."""
 import pytest
 import smtplib
+from contextlib import contextmanager
 
 
-@pytest.fixture(scope="module", autouse=True)
-def smtp_connection() -> smtplib.SMTP:
+# @pytest.fixture(scope="module", autouse=True)
+# def smtp_connection() -> smtplib.SMTP:
+#     """
+#     A fixture to create an SMTP connection.
+
+#     Returns:
+#         An SMTP connection
+#     """
+#     print("SMTP Connection fixture start")
+#     yield smtplib.SMTP("smtp.gmail.com", 587, timeout=60)
+#     print("SMTP Connection Tear Down")
+
+
+# Demonstrate how to share a base fixture with different scopes
+@contextmanager
+def smtp_connection_base() -> smtplib.SMTP:
     """
     A fixture to create an SMTP connection.
 
@@ -14,3 +29,10 @@ def smtp_connection() -> smtplib.SMTP:
     print("SMTP Connection fixture start")
     yield smtplib.SMTP("smtp.gmail.com", 587, timeout=60)
     print("SMTP Connection Tear Down")
+
+
+# Fixture with module scope
+@pytest.fixture(scope="module")
+def smtp_connection():
+    with smtp_connection_base() as result:
+        yield result
